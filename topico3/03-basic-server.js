d8020481d5d8cbd43df8defc1d5e2d727f832227
HeloisaSoaresUtpr <helo_soarescosta@hotmail.com>
2021-04-14 00:44:17 -0300

    callback

--- topico3/03-basic-server.js
+++ topico3/03-basic-server.js
@@ -1,19 +1,28 @@
-const fs = require('fs');
+//** Exercício 1: para o arquivo em "https://github.com/andreendo/topicos-computacao/blob/master/topico3/03-basic-server.js",
+// refatore o callback para o GET usar async-await na leitura do arquivo.
+
+const fs = require('fs').promises;
 const express = require('express'); 
 
 const app = express();
 
 app.get('/', (req, res) => {
+
+    let arquivo = '';
     console.log('some request');
 
     // leitura async libera o event loop para processar outras requisicoes
-    fs.readFile('./html/basic.html', 'utf-8', (err, data) => {
-        if (err) {
-            res.send(err);
-            return;
-        } 
-        res.send(data);
-    });
+    (async () => {
+        console.log('ler arquivo')
+        try {
+            arquivo = await fs.readFile('./html/basic.html', 'utf-8')
+        }
+        catch(erro){
+           arquivo =  'erro ao carregar'
+       }
+       res.send(arquivo);
+    })();
+    
 });
 
 app.listen(3000, () => {
