67da46827af9e14fb3fcc72d0703817dcf54c886
HeloisaSoaresUtpr <helo_soarescosta@hotmail.com>
2021-04-19 00:01:21 -0300

    timers

---
+++ topico3/integracao/timers2.js
@@ -0,0 +1,38 @@
+// timers
+
+//setImmediate(() => {
+//    console.log('setImediate cb 1');
+//});
+
+//setImmediate(() => {
+//    console.log('setImediate cb 2');
+//});
+
+process.nextTick(() = > {
+    console.log(next tick 1);
+});
+
+process.nextTick(() = > {
+    console.log(next tick 2);
+});
+
+/*setTimeout(() => {
+    console.log('1s depois');
+}.1000;
+
+setTimeout(() => {
+    console.log('2s depois');
+}.2000;
+let c = 0;*/
+
+const refInterval = setInterval(() => {
+    console.log('interval', c++);
+}, 100);
+
+setTimeout(() => {
+    clearInterval(refInterval);
+    console.log("end interval");
+}, 14000);
+
+
+console.log('end of script');
